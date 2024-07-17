698d1d4ab089a098222717aa49a279b7b9019151
ornops <joshuaornopia@gmail.com>
2024-07-17 14:14:09 +0800

    Updated

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import {getAuth,signInWithRedirect,signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+import {getAuth,signInWithRedirect,signInWithPopup, GoogleAuthProvider,createUserWithEmailAndPassword,} from 'firebase/auth';
 import {getFirestore, doc, getDoc,setDoc} from 'firebase/firestore'
 
 
@@ -14,24 +14,23 @@ const firebaseConfig = {
   
   // Initialize Firebase
   const firebaseApp = initializeApp(firebaseConfig);
-  const provider = new GoogleAuthProvider();
-    provider.setCustomParameters({
+  const googleProvider = new GoogleAuthProvider();
+  googleProvider.setCustomParameters({
         prompt: "select_account"
     });
 
     export const auth = getAuth();
-    export const signInWithGooglePopup = () => signInWithPopup(auth,provider)
+    export const signInWithGooglePopup = () => signInWithPopup(auth,googleProvider)
+
 
     export const db = getFirestore();
     
-    export const createUserDocumentFromAuth = async (userAuth) =>{
+    export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) =>{
+        if(!userAuth) return;
         const userDocRef = doc(db,'users', userAuth.uid);
 
-        console.log(userDocRef);
-
         const userSnapshot = await getDoc(userDocRef);
-        console.log(userSnapshot);
-        console.log(userSnapshot.exists());
+
 
         if(!userSnapshot.exists()){
             const {displayName, email} = userAuth;
@@ -40,7 +39,8 @@ const firebaseConfig = {
                 await setDoc(userDocRef,{
                     displayName,
                     email,
-                    createdAt
+                    createdAt,
+                    ...additionalInformation,
                 });
             } catch (error) {
                 console.log('error creating the user', error.message);
@@ -49,4 +49,10 @@ const firebaseConfig = {
         return userDocRef;
 
 
+    }
+
+    export const createAuthUserWithEmailAndPassword = async (email,password) =>{
+        if(!email || !password) return;
+
+        return await createUserWithEmailAndPassword(auth,email,password)
     }
